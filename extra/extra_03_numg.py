8450bd0d86efa86af94febdf0401edd4d5103c84
Seungdo Choi <mitch00bot@gmail.com>
2019-06-16 14:29:07 +0900

    반복문 학습 계속

--- extra/extra_03_numg.py
+++ extra/extra_03_numg.py
@@ -5,11 +5,13 @@
 
 '''
 1. 컴터가 난수를 생성한다. (1~100 사이.)
--> rand 이용? 메소드?
+-> cf. rand 이용? 메소드? 검색 완료.
 2. 사용자로부터 숫자를 입력받는다.
 3. 입력받은 숫자가 난수와 같은지 비교한다/
 4. 같으면 정답, 틀리면 오답.
--> 기회를 얼만큼 줘야? 단판승?
+-> 기회를 얼만큼 줘야? 단판승? Y.
+-> 역시 이게 개선점. (Cf. hongsam)
+
 
 '''
 '''
@@ -26,13 +28,3 @@ else:
     print("False") 
 # by SD
 '''
-
-
-answer = random.randint(1, 100)
-print(answer)
-your_answer = input("Type in a num.")
-
-if answer == your_answer:
-    print("True")
-else:
-    print("Wrong")
